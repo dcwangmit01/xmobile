269f22843c8629714ad83188916939bdd12465e3
David Wang <dcwangmit01@gmail.com>
2012-12-24 19:59:45 -0800

    * Added .gitignore * Added some doc/notes.txt * Deleted two index.html files that probably should not have been checked in (autogenerated?) * Added a bunch of javascript libraries and resources in the lib directory   - Bootstrap, backbone, jquery, underscore   - d3, nvD3   - Glyphicons   - A creative commons photo * Implemented a working website with ios and android app support

---
+++ www/js/main.js
@@ -0,0 +1,75 @@
+var AppRouter = Backbone.Router.extend({
+
+	routes: {
+	    ""                 : "home",
+	    "about"             : "about",
+	    "graph"             : "graph"
+	},
+
+	initialize: function () {
+	    document.addEventListener('deviceready', function() {
+		    window.utils.showAlert("Device", "Detected", "alert-success");
+		});
+	    
+	    this.navbarView = new NavigationView({ templateSelector: '#navbarview',
+						   targetSelector: '#navbar' });
+	    this.navbarView.render();
+
+	    this.sidebarView = new NavigationView({ templateSelector: '#sidebarview',
+						    targetSelector: '#sidebar' });
+	    this.sidebarView.render();
+	},
+    
+	home: function () {
+	    if (!this.homeView) {
+		this.homeView = new ContentView({ templateSelector: '#homeview',
+						  targetSelector: '#content' });
+	    }
+	    this.homeView.render();
+	    this.navbarView.selectMenuItem('home');
+	    this.sidebarView.selectMenuItem('home');
+	},
+    
+	about: function () {
+	    if (!this.aboutView) {
+		this.aboutView = new ContentView({ templateSelector: '#aboutview',
+						   targetSelector: '#content' });
+	    }
+	    this.aboutView.render();
+	    this.navbarView.selectMenuItem('about');
+	    this.sidebarView.selectMenuItem('about');
+	},
+
+	graph: function () {
+	    if (!this.graphView) {
+		this.graphView = new ContentView({ templateSelector: '#graphview',
+						   targetSelector: '#content' });
+	    }
+	    this.graphView.render();
+	    this.navbarView.selectMenuItem('graph');
+	    this.sidebarView.selectMenuItem('graph');
+	},
+
+    });
+
+{
+    // Load the client side include templates
+    var deferreds = [];
+    $(".client-side-include").each(function(){
+            var inc=$(this);
+            deferreds.push(
+			   $.get(inc.attr("src"), function(data){ inc.replaceWith(data); })
+			   );
+	});
+    
+    // Start the Backbone Application
+    $.when.apply(null, deferreds).done(function() {
+	    window.utils.showAlert("Device", "Detection In Progress", "alert-info");
+	    window.setTimeout(function() { 
+		    window.setTimeout(function() { window.utils.hideAlert() });
+		}, 10000);
+	    
+	    app = new AppRouter();
+            Backbone.history.start();
+        });        
+}    
---
+++ www/js/views.js
@@ -0,0 +1,32 @@
+window.ContentView = Backbone.View.extend({
+	
+	initialize: function (options) {
+	    this.options = options;
+	},
+	
+	render: function () {
+	    var template = $(this.options.templateSelector).text();
+	    var processed = _.template(template, null);
+	    $(this.options.targetSelector).html(processed);
+	},
+	
+	selectMenuItem: function (menuItem) {
+	    $('.nav li').removeClass('active');
+	    if (menuItem) {
+		$('.' + menuItem).addClass('active');
+	    }
+	}
+
+    });
+
+
+window.NavigationView = window.ContentView.extend({
+	
+	selectMenuItem: function (menuItem) {
+	    $('.nav li').removeClass('active');
+	    if (menuItem) {
+		$('.' + menuItem).addClass('active');
+	    }
+	}
+	
+    });
